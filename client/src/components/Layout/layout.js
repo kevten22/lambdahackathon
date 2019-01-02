1f962bb47eb38997fb8d75183cbf62cec9a2c368
Josue Peralta <jperal77@gmail.com>
2019-01-02 16:29:39 -0600

    removed unnecessary files and imported layout scss file into layout component

--- client/src/components/Layout/layout.js
+++ client/src/components/Layout/layout.js
@@ -2,6 +2,7 @@ import React from 'react';
 import Footer from './footer';
 import Header from './header';
 import 'bootstrap/dist/css/bootstrap-grid.min.css';
+import './layout.scss';
 import 'typeface-roboto';
 
 const Layout = ({ children }) => {
