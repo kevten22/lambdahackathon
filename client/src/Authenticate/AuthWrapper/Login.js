7fcfa99461f61abfd6aba3146a0a9772697654b0
Josue Peralta <jperal77@gmail.com>
2019-01-03 14:44:17 -0600

    added homepage styling

--- client/src/Authenticate/AuthWrapper/Login.js
+++ client/src/Authenticate/AuthWrapper/Login.js
@@ -1,12 +1,11 @@
-import React, { Component } from "react";
-import { FormInput, FormContainer } from "../../Styling/ReusableStyles";
+import React, { Component } from 'react';
 
 class Login extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      usernameInput: "",
-      passwordInput: ""
+      usernameInput: '',
+      passwordInput: ''
     };
   }
   editLoginHandler = e => {
@@ -19,28 +18,29 @@ class Login extends Component {
   };
   render() {
     return (
-      <FormContainer>
-        <div>Login Component</div>
-        <FormInput action="" onSubmit={this.handleLoginSubmitForm(this.props)}>
-          <input
-            type="text"
-            name="usernameInput"
-            onChange={this.editLoginHandler}
-            placeholder="username"
-            value={this.state.usernameInput}
-            required
-          />
-          <input
-            type="password"
-            name="passwordInput"
-            onChange={this.editLoginHandler}
-            placeholder="password"
-            value={this.state.passwordInput}
-            required
-          />
-          <button>Login</button>
-        </FormInput>
-      </FormContainer>
+      <form
+        className="login-form"
+        action=""
+        onSubmit={this.handleLoginSubmitForm(this.props)}
+      >
+        <input
+          type="text"
+          name="usernameInput"
+          onChange={this.editLoginHandler}
+          placeholder="username"
+          value={this.state.usernameInput}
+          required
+        />
+        <input
+          type="password"
+          name="passwordInput"
+          onChange={this.editLoginHandler}
+          placeholder="password"
+          value={this.state.passwordInput}
+          required
+        />
+        <button>Login</button>
+      </form>
     );
   }
 }
--- client/src/Authenticate/AuthWrapper/Register.js
+++ client/src/Authenticate/AuthWrapper/Register.js
@@ -1,12 +1,11 @@
-import React, { Component } from "react";
-import { FormInput, FormContainer } from "../../Styling/ReusableStyles";
+import React, { Component } from 'react';
 
 class Register extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      usernameInput: "",
-      passwordInput: ""
+      usernameInput: '',
+      passwordInput: ''
     };
   }
   editRegisterHandler = e => {
@@ -20,31 +19,25 @@ class Register extends Component {
   };
   render() {
     return (
-      <FormContainer>
-        <div>Register Component</div>
-        <FormInput
-          action=""
-          onSubmit={this.handleRegisterSubmitForm(this.props)}
-        >
-          <input
-            type="text"
-            name="usernameInput"
-            onChange={this.editRegisterHandler}
-            placeholder="username"
-            value={this.state.usernameInput}
-            required
-          />
-          <input
-            type="password"
-            name="passwordInput"
-            onChange={this.editRegisterHandler}
-            placeholder="password"
-            value={this.state.passwordInput}
-            required
-          />
-          <button>Register</button>
-        </FormInput>
-      </FormContainer>
+      <form action="" onSubmit={this.handleRegisterSubmitForm(this.props)}>
+        <input
+          type="text"
+          name="usernameInput"
+          onChange={this.editRegisterHandler}
+          placeholder="username"
+          value={this.state.usernameInput}
+          required
+        />
+        <input
+          type="password"
+          name="passwordInput"
+          onChange={this.editRegisterHandler}
+          placeholder="password"
+          value={this.state.passwordInput}
+          required
+        />
+        <button>Register</button>
+      </form>
     );
   }
 }
