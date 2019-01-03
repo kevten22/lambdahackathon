92928e90094badc7eb48803bcdb163711a368838
Kevin Tena <kevten09@gmail.com>
2019-01-03 15:55:10 -0600

    Added endpoints

--- JSServer/server.js
+++ JSServer/server.js
@@ -1,3 +1,206 @@
 const express = require('express');
 
 const server = express();
+const bodyParser = require('body-parser');
+
+server.use(bodyParser.json());
+
+server.get('/users', async (req, res) => {
+    try {
+        
+    } catch (err) {
+
+    }
+});
+
+server.get('/users/:id', async (req, res) => {
+    try {
+        
+    } catch (err) {
+
+    }
+});
+
+server.get('/users/:id/collections' , async (req, res) => {
+    try {
+        
+    } catch (err) {
+
+    }
+});
+
+server.get('/collections', async (req, res) => {
+    try {
+        
+    } catch (err) {
+
+    }
+});
+
+server.get('/images', async (req, res) => {
+    try {
+        
+    } catch (err) {
+
+    }
+});
+
+server.get('/images/:id', async (req, res) => {
+    try {
+        
+    } catch (err) {
+
+    }
+});
+
+
+serve.post('/users', async (req, res) => {
+    try {
+        
+    } catch (err) {
+
+    }
+});
+
+serve.post('/users/:id', async (req, res) => {
+    try {
+        
+    } catch (err) {
+
+    }
+});
+
+serve.post('/users/:id/collections' , async (req, res) => {
+    try {
+        
+    } catch (err) {
+
+    }
+});
+
+serve.post('/collections', async (req, res) => {
+    try {
+        
+    } catch (err) {
+
+    }
+});
+
+serve.post('/images', async (req, res) => {
+    try {
+        
+    } catch (err) {
+
+    }
+});
+
+serve.post('/images/:id', async (req, res) => {
+    try {
+        
+    } catch (err) {
+
+    }
+});
+
+server.put('/users', async (req, res) => {
+    try {
+        
+    } catch (err) {
+
+    }
+});
+
+server.put('/users/:id', async (req, res) => {
+    try {
+        
+    } catch (err) {
+
+    }
+});
+
+server.put('/users/:id/collections' , async (req, res) => {
+    try {
+        
+    } catch (err) {
+
+    }
+});
+
+server.put('/collections', async (req, res) => {
+    try {
+        
+    } catch (err) {
+
+    }
+});
+
+server.put('/images', async (req, res) => {
+    try {
+        
+    } catch (err) {
+
+    }
+});
+
+server.put('/images/:id', async (req, res) => {
+    try {
+        
+    } catch (err) {
+
+    }
+});
+//
+server.delete('/users', async (req, res) => {
+    try {
+        
+    } catch (err) {
+
+    }
+});
+
+server.delete('/users/:id', async (req, res) => {
+    try {
+        
+    } catch (err) {
+
+    }
+});
+
+server.delete('/users/:id/collections' , async (req, res) => {
+    try {
+        
+    } catch (err) {
+
+    }
+});
+
+server.delete('/collections', async (req, res) => {
+    try {
+        
+    } catch (err) {
+
+    }
+});
+
+server.delete('/images', async (req, res) => {
+    try {
+        
+    } catch (err) {
+
+    }
+});
+
+server.delete('/images/:id', async (req, res) => {
+    try {
+        
+    } catch (err) {
+
+    }
+});
+
+
+
+
+
+
+server.listen(8000, () => console.log('API running on port 8000'));
