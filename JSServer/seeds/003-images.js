c933946694509f749f706cb16cd9aad5369715ae
jeffyjkang <jeffyjkang@gmail.com>
2019-01-03 23:17:40 -0800

    update images seed

--- JSServer/seeds/003-images.js
+++ JSServer/seeds/003-images.js
@@ -7,83 +7,129 @@ exports.seed = function(knex, Promise) {
       return knex("images").insert([
         {
           id: 1,
-          imageURL: "rowValue1",
-          questionParam: "question",
+          imageURL:
+            "https://github.com/kevten22/lambdahackathon/blob/master/JSServer/ImageData/birds/3950_ext_01_0.jpg?raw=true",
+          questionParam: "is this a cat?",
           trueOption: 0,
           falseOption: 0,
           collectionsID: 1
         },
         {
           id: 2,
-          imageURL: "rowValue2",
-          questionParam: "question",
+          imageURL:
+            "https://github.com/kevten22/lambdahackathon/blob/master/JSServer/ImageData/birds/51DDQ3AGbnL.jpg?raw=true",
+          questionParam: "is this a cat?",
           trueOption: 0,
           falseOption: 0,
-          collectionsID: 2
+          collectionsID: 1
         },
         {
           id: 3,
-          imageURL: "rowValue3",
-          questionParam: "question",
+          imageURL:
+            "https://github.com/kevten22/lambdahackathon/blob/master/JSServer/ImageData/birds/51j1HFdKEJL._SS500.jpg?raw=true",
+          questionParam: "is this a cat?",
           trueOption: 0,
           falseOption: 0,
-          collectionsID: 3
+          collectionsID: 1
         },
         {
           id: 4,
-          imageURL: "rowValue3",
-          questionParam: "question",
+          imageURL:
+            "https://github.com/kevten22/lambdahackathon/blob/master/JSServer/ImageData/birds/Ruby-website-min-500x500.jpg?raw=true",
+          questionParam: "is this a cat?",
           trueOption: 0,
           falseOption: 0,
           collectionsID: 1
         },
         {
           id: 5,
-          imageURL: "rowValue3",
-          questionParam: "question",
-          trueOption: 0,
-          falseOption: 0,
-          collectionsID: 5
-        },
-        {
-          id: 6,
-          imageURL: "rowValue3",
-          questionParam: "question",
-          trueOption: 0,
-          falseOption: 0,
-          collectionsID: 5
-        },
-        {
-          id: 7,
-          imageURL: "rowValue3",
-          questionParam: "question",
-          trueOption: 0,
-          falseOption: 0,
-          collectionsID: 6
-        },
-        {
-          id: 8,
-          imageURL: "rowValue3",
-          questionParam: "question",
-          trueOption: 0,
-          falseOption: 0,
-          collectionsID: 4
-        },
-        {
-          id: 9,
-          imageURL: "rowValue3",
-          questionParam: "question",
+          imageURL:
+            "https://github.com/kevten22/lambdahackathon/blob/master/JSServer/ImageData/birds/images.jpeg?raw=true",
+          questionParam: "is this a cat?",
           trueOption: 0,
           falseOption: 0,
           collectionsID: 1
         },
         {
-          id: 10,
-          imageURL: "rowValue3",
-          questionParam: "question",
+          id: 6,
+          imageURL:
+            "https://github.com/kevten22/lambdahackathon/blob/master/JSServer/ImageData/birds/portabella1.jpg?raw=true",
+          questionParam: "is this a cat?",
           trueOption: 0,
           falseOption: 0,
-          collectionsID: 3
+          collectionsID: 1
+        },
+        {
+          id: 7,
+          imageURL:
+            "https://github.com/kevten22/lambdahackathon/blob/master/JSServer/ImageData/birds/test.jpg?raw=true",
+          questionParam: "is this a cat?",
+          trueOption: 0,
+          falseOption: 0,
+          collectionsID: 1
+        },
+        {
+          id: 8,
+          imageURL:
+            "https://github.com/kevten22/lambdahackathon/blob/master/JSServer/ImageData/dogs/3876_ext_01_0.jpg?raw=true",
+          questionParam: "is this a dog?",
+          trueOption: 0,
+          falseOption: 0,
+          collectionsID: 2
+        },
+        {
+          id: 9,
+          imageURL:
+            "https://github.com/kevten22/lambdahackathon/blob/master/JSServer/ImageData/dogs/Instant-Pot-Spaghetti-4-of-5.jpg?raw=true",
+          questionParam: "is this a dog?",
+          trueOption: 0,
+          falseOption: 0,
+          collectionsID: 2
+        },
+        {
+          id: 10,
+          imageURL:
+            "https://github.com/kevten22/lambdahackathon/blob/master/JSServer/ImageData/dogs/Lentil-Carrot-Hot-Dogs-4B-500x500.jpg?raw=true",
+          questionParam: "is this a dog?",
+          trueOption: 0,
+          falseOption: 0,
+          collectionsID: 2
+        },
+        {
+          id: 11,
+          imageURL:
+            "https://github.com/kevten22/lambdahackathon/blob/master/JSServer/ImageData/dogs/hot-dog.jpeg?raw=true",
+          questionParam: "is this a dog?",
+          trueOption: 0,
+          falseOption: 0,
+          collectionsID: 2
+        },
+        {
+          id: 12,
+          imageURL:
+            "https://github.com/kevten22/lambdahackathon/blob/master/JSServer/ImageData/dogs/people_page_blaine-still.jpg?raw=true",
+          questionParam: "is this a dog?",
+          trueOption: 0,
+          falseOption: 0,
+          collectionsID: 2
+        },
+        {
+          id: 13,
+          imageURL:
+            "https://github.com/kevten22/lambdahackathon/blob/master/JSServer/ImageData/dogs/safety-serve-articles-driving-at-night.jpg?raw=true",
+          questionParam: "is this a dog?",
+          trueOption: 0,
+          falseOption: 0,
+          collectionsID: 2
+        },
+        {
+          id: 14,
+          imageURL:
+            "https://github.com/kevten22/lambdahackathon/blob/master/JSServer/ImageData/dogs/safety-serve-articles-driving-at-night.jpg?raw=true",
+          questionParam: "is this a dog?",
+          trueOption: 0,
+          falseOption: 0,
+          collectionsID: 2
         }
       ]);
     });
