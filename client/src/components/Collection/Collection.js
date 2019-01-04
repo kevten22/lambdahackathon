fe378c779670f5ecaecd0cd6dcc5f5a0ac414dfe
Josue Peralta <jperal77@gmail.com>
2019-01-04 02:04:15 -0600

    styled the annotate view

--- client/src/components/Collection/Collection.js
+++ client/src/components/Collection/Collection.js
@@ -1,46 +1,65 @@
 import React, { Component } from 'react';
-import {Route,Link} from "react-router-dom";
-import axios from "axios"
+import { Route, Link } from 'react-router-dom';
+import { Container, Row, Col } from 'reactstrap';
+import axios from 'axios';
 import './Collection.scss';
-import Annotation from "../Annotation/annotation"
+import Annotation from '../Annotation/annotation';
 
 class Collection extends Component {
   constructor(props) {
     super(props);
     this.state = {
-        collection:props.match.params.id,
-        urlArray:[],
-        clicked:false
+      collection: props.match.params.id,
+      urlArray: [],
+      clicked: false
     };
   }
 
-  componentDidMount(){
-      axios
+  componentDidMount() {
+    axios
       .get(`http://localhost:9000/images/${this.state.collection}`)
       .then(response => {
-          console.log(response);
-          this.setState({urlArray:response.data})
+        console.log(response);
+        this.setState({ urlArray: response.data });
       })
       .catch(error => {
-          console.log(error)
-      })
+        console.log(error);
+      });
   }
 
   handleClick = () => {
-      this.setState({clicked:true})
-  }
-
+    this.setState({ clicked: true });
+  };
 
   render() {
-    const button = <div>Would you like to annotate this collection?<Link onClick={this.handleClick}to={`${this.props.match.url}/annotate`}>Annotate</Link></div>
-    const annotationLink = (this.state.clicked ? null : button)
+    const button = (
+      <div className="annotate-confirm">
+        <p>Would you like to annotate this collection?</p>
+        <Link
+          onClick={this.handleClick}
+          to={`${this.props.match.url}/annotate`}
+        >
+          Annotate
+        </Link>
+      </div>
+    );
+    const annotationLink = this.state.clicked ? null : button;
     return (
-        <section className="collection">
-            <div>Collection Name: {this.state.collection}</div>
-            <div>Labeling Instructions:</div>
+      <Container className="collection">
+        <Row>
+          <Col className="collection-header" xs="12">
+            <h1>Collection: {this.state.collection}</h1>
+            <p>Labeling Instructions: Lorem, ipsum dolor sit amet</p>
             {annotationLink}
-            <Route path={`${this.props.match.url}/annotate`} render={props => <Annotation {...props} array={this.state.urlArray}/>}/>
-        </section>
+          </Col>
+          <Route
+            path={`${this.props.match.url}/annotate`}
+            render={props => (
+              <Annotation {...props} array={this.state.urlArray} />
+            )}
+          />
+        </Row>
+      </Container>
     );
   }
 }
