d668b1048c56a1ec6b30f42ef380b9d00fca9319
dan2014 <dmen326@gmail.com>
2019-01-04 04:57:26 -0500

    completed the put requests

--- client/src/components/Annotation/carousel.js
+++ client/src/components/Annotation/carousel.js
@@ -1,10 +1,9 @@
 import React from 'react';
 
 const Carousel = (props) => {
-    const url = "http://localhost:9000/images/"
     return(
         <div className="carousel">
-            <img src={`${url}${props.image}`} alt="#"/>
+            <img src={props.image} alt="#"/>
         </div>
     );
     };
