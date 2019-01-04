b138792867a211ec1a1590bb176fe9b2006feb18
Josue Peralta <jperal77@gmail.com>
2019-01-04 14:30:45 -0600

    added some styling to View Your Data page

--- client/src/components/ViewData/ViewData.js
+++ client/src/components/ViewData/ViewData.js
@@ -7,57 +7,57 @@ class ViewData extends Component {
   constructor(props) {
     super(props);
     this.state = {
-        collectionID:props.id,
-        collection:props.match.params.id,
-        question:"",
-        urlArray:[],
-        objArray:[]
+      collectionID: props.id,
+      collection: props.match.params.id,
+      question: '',
+      urlArray: [],
+      objArray: []
     };
   }
 
   componentDidMount() {
-      axios
-      .get(`http://localhost:9000/images/collections/${this.state.collectionID}`)
+    axios
+      .get(
+        `http://localhost:9000/images/collections/${this.state.collectionID}`
+      )
       .then(response => {
-          console.log(response);
-          let q=response.data[0];
-          q = q.questionParam;
-          this.setState({
-              objArray:response.data,
-              urlArray:response.data.map(ele => ele.imageURL),
-              question:q
-            })
+        console.log(response);
+        let q = response.data[0];
+        q = q.questionParam;
+        this.setState({
+          objArray: response.data,
+          urlArray: response.data.map(ele => ele.imageURL),
+          question: q
+        });
       })
       .catch(error => {
-          console.log(error)
-      })
+        console.log(error);
+      });
   }
 
-
   render() {
-
     return (
-      <Container className="collection">
+      <Container className="view-data">
         <Row>
-          <Col className="collection-header" xs="12">
+          <Col className="view-data-header" xs="12">
             <h1>Collection: {this.state.collection}</h1>
-            <p>Number of Images in this dataset: {this.state.objArray.length}</p>
+            <p>
+              Number of Images in this dataset: {this.state.objArray.length}
+            </p>
             <p>Labeling Instructions: {this.state.question}</p>
-
           </Col>
+          {this.state.objArray.map((e, index) => (
+            <Col className="collection-summary" xs="6" key={index}>
+              <Col sm="6">
+                <img src={e.imageURL} alt="#" />
+              </Col>
+              <Col sm="5">
+                <p>True:{e.trueOption}</p>
+                <p>False:{e.falseOption}</p>
+              </Col>
+            </Col>
+          ))}
         </Row>
-        {
-            this.state.objArray.map((e,index) => {
-                const summary = <Row key={index}>
-                    <Col><img style={{"maxWidth":"100px"}}src={e.imageURL}alt="#" /></Col>
-                    <Col>
-                        <Row>True:{e.trueOption}</Row>
-                        <Row>False:{e.falseOption}</Row>
-                    </Col>
-                </Row>
-                return summary
-            })
-        }
       </Container>
     );
   }
